52d2672c9c65196cd6696c5282382f2370fb731b
niravdsouza <niravdsouza@gmail.com>
2017-04-26 18:32:24 -0400

    OK

---
+++ SDN/startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+N=`cat ./reference/configs.conf | grep N= | cut -d= -f2`
+
+for int in $(cat ./reference/configs.conf | grep interfaces= | cut -d= -f2 | sed 's/,/ /g')
+do
+	current_rx=`ifconfig $int | grep bytes | awk '{print $2}' | cut -d: -f2`
+	current_tx=`ifconfig $int | grep bytes | awk '{print $6}' | cut -d: -f2`
+
+	echo "RX:$current_rx" > ./reference/previous_bytes.$int
+	echo "TX:$current_tx" >> ./reference/previous_bytes.$int
+
+	> ./history/historicaldata.$int
+	for i in $(seq 1 $N)
+	do
+		echo 0 >> ./history/historicaldata.$int
+	done
+done
+
